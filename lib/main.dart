3b90c5e31fbf5bbcdb777b4c5e528e9ea77b3897
Hennoshin <m.irfan.hilmi@gmail.com>
2022-05-10 19:22:49 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,156 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:gopharma/firebase_options.dart';
+import 'package:provider/provider.dart';
+import 'HomePage.dart';
+import 'screens/register/signup.dart';
+import 'routes_name.dart';
+import 'viewmodel/register_viewmodel.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
-
   runApp(const MyApp());
+}
+
+Route<dynamic>? createRoute(settings) {
+  switch (settings.name) {
+    case routeHome:
+      return MaterialPageRoute(builder: (context) => HomePage());
+    case routeLogin:
+      return MaterialPageRoute(builder: (context) => const LoginDemo());
+    case routeRegister:
+      return MaterialPageRoute(builder: (context) => ChangeNotifierProvider(create: (_) => RegisterViewModel(), child: const SignUp()));
+  }
+
+  return null;
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  final String initialRoute = routeLogin;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: true,
+      initialRoute: initialRoute,
+      onGenerateRoute: createRoute,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginDemo extends StatefulWidget {
+  const LoginDemo({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _LoginDemoState createState() => _LoginDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _LoginDemoState extends State<LoginDemo> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.cyan.shade300,
+      
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 400,
+                    height: 300,
+                    child: Image.asset('asset/images/login.png')),
+                    
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 100, right: 100, top: 15, bottom: 0
+              ),
+              child: TextField(
+                textAlign: TextAlign.center,      
+                decoration: InputDecoration(
+                     border: OutlineInputBorder( 
+                       borderRadius: BorderRadius.circular(10.0),
+                       borderSide: BorderSide.none
+                       ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    label: const Center(
+                      child: Text('Username'),
+                    ),
+                    hintText: 'Input Username'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 100, right: 100, top: 15, bottom: 20),
+              child: TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder( 
+                       borderRadius: BorderRadius.circular(10.0),
+                       borderSide: BorderSide.none
+                       ),
+                       filled: true,
+                    fillColor: Colors.white,
+                    label: const Center(
+                      child: Text('Password'),
+                    ),
+                    hintText: 'Input Password'),
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
+                Navigator.pushReplacementNamed(context, routeRegister);
+              },
+            
+              child: Text(
+                "Don't Have an Account? Register",
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
+                 //belum
+              },
+              child: Text(
+                'Forgot Your Password',
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              margin: const EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  color: Colors.pink, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+
+              onPressed: (){
+                 Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => HomePage()));
+              },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 130,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
