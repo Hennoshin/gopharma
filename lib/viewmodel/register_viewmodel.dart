a2ee8faf4a265dadb65e3b0a30d612a94b39db46
Hennoshin <m.irfan.hilmi@gmail.com>
2022-05-10 19:22:11 +0700

    Add registration view model model

---
+++ lib/viewmodel/register_viewmodel.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import '../model/registration_form.dart';
+
+class RegisterViewModel extends ChangeNotifier {
+  RegistrationForm form = RegistrationForm();
+
+  bool _isEmailValid = false;
+  bool _isPasswordValid = false;
+  bool _isConfirmPasswordValid = false;
+
+  bool get isEmailValid => _isEmailValid;
+  bool get isPasswordValid => _isPasswordValid;
+  bool get isConfirmPasswordValid => _isConfirmPasswordValid;
+
+  bool _validateForm() {
+    return _isEmailValid && _isPasswordValid && _isConfirmPasswordValid;
+  }
+
+  void setEmail(String email) {
+    _isEmailValid = form.setEmail(email);
+
+    notifyListeners();
+  }
+
+  void setPassword(String password) {
+    _isPasswordValid = form.setPassword(password);
+    _isConfirmPasswordValid = form.setConfirmPassword(form.confirmPassword ?? "");
+
+    notifyListeners();
+  }
+
+  void setConfirmPassword(String confirmPassword) {
+    _isConfirmPasswordValid = form.setConfirmPassword(confirmPassword);
+
+    notifyListeners();
+  }
+
+  Future<String?> registerUser() async {
+
+    // TODO: Move this operation to repository, not supposed to be ViewModel responsibility
+    FirebaseAuth auth = FirebaseAuth.instance;
+    try {
+      final credential = await auth.createUserWithEmailAndPassword(email: form.email!, password: form.password!);
+    } on FirebaseAuthException catch (error) {
+      return Future.value(error.message);
+    }
+
+    FirebaseAuth.instance.currentUser!.sendEmailVerification();
+    return Future.value(null);
+  }
+}
