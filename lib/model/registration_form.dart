cc06d38699648c483c7ea1da3321d5d81802d004
Hennoshin <m.irfan.hilmi@gmail.com>
2022-05-10 19:21:35 +0700

    Add registration form model

---
+++ lib/model/registration_form.dart
@@ -0,0 +1,41 @@
+class RegistrationForm {
+  String? _email;
+  String? _password, _confirmPassword;
+
+  String? _firstName, _lastName;
+
+  bool setEmail(String email) {
+    final emailRegex = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+
+    if (!emailRegex.hasMatch(email)) {
+      return false;
+    }
+
+    _email = email;
+    return true;
+  }
+
+  bool setPassword(String password) {
+    if (password.length < 8) {
+      return false;
+    }
+
+    _password = password;
+    return true;
+  }
+
+  bool setConfirmPassword(String confirmPassword) {
+    if (confirmPassword != _password) {
+      return false;
+    }
+
+    _confirmPassword = confirmPassword;
+    return true;
+  }
+
+  String? get email => _email;
+  String? get password => _password;
+  String? get confirmPassword => _confirmPassword;
+  String? get firstName => _firstName;
+  String? get lastName => _lastName;
+}
