7a165bdbf7992a32ebf2f1e516410f792faebb87
Hennoshin <m.irfan.hilmi@gmail.com>
2022-05-11 21:26:40 +0700

    Modify otp.dart to be cleaner

--- lib/screens/register/otp.dart
+++ lib/screens/register/otp.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
-import '../../main.dart';
+import '../../viewmodel/otp_viewmodel.dart';
+import 'package:provider/provider.dart';
 import 'signup.dart';
 import 'package:otp_text_field/otp_field.dart';
 import 'package:otp_text_field/style.dart';
 
 
-class OTP extends StatefulWidget {
-  @override
-  _OTP createState() => _OTP();
-}
+class OTP extends StatelessWidget {
+  const OTP({Key? key}) : super(key: key);
 
-class _OTP extends State<OTP> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.cyan.shade300,
        appBar: AppBar(
-        title: Text("OTP Verification"),
+        title: const Text("OTP Verification"),
         backgroundColor: Colors.cyan.shade400,
       ),
       
@@ -26,23 +24,21 @@ class _OTP extends State<OTP> {
             Padding(
               padding: const EdgeInsets.only(top: 20.0),
               child: Center(
-                child: Container(
+                child: SizedBox(
                     width: 100,
                     height: 100,
                     child: Image.asset('asset/images/login.png')),
                     
               ),
             ),
-             Padding(
-              padding: const EdgeInsets.only(
+             const Padding(
+              padding: EdgeInsets.only(
                 left: 100, right: 100, top: 15, bottom: 0
               ),
               
               child: Center(
-                child: Container(
-                  child: Text("We've sent OTP Number Through Your Input Email",
-                  textAlign: TextAlign.center),
-                ),
+                child: Text("We've sent OTP Number Through Your Input Email",
+                textAlign: TextAlign.center),
               ),
              ),
             Padding(
@@ -67,7 +63,7 @@ class _OTP extends State<OTP> {
                   length: 6,
                   width: MediaQuery.of(context).size.width,
                   
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontSize: 17
                   ),
                   textFieldAlignment: MainAxisAlignment.spaceAround,
@@ -77,43 +73,45 @@ class _OTP extends State<OTP> {
                   },
                 ),
             ),
-           Padding(
-             padding: const EdgeInsets.only(
+           const Padding(
+             padding: EdgeInsets.only(
                bottom: 10
              ),
            ), 
-           FlatButton(
+           TextButton(
               onPressed: (){
                  Navigator.push(
                       context, MaterialPageRoute(builder: (_) => SignUp ()));
               },
-              child: Text(
+              child: const Text(
                 " Wrong Email? Change Email",
                 style: TextStyle(color: Colors.black, fontSize: 15),
               ),
             ),
-            Padding(
-             padding: const EdgeInsets.only(
+            const Padding(
+             padding: EdgeInsets.only(
                bottom: 130
              ),
            ), 
-            Container(
+            SizedBox(
               height: 50,
               width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.pink, borderRadius: BorderRadius.circular(20)),
-              child: FlatButton(
-               onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => LoginDemo()));
+              child: Consumer<OTPViewModel>(
+                builder: (context, viewModel, child) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.pink),
+                    onPressed: viewModel.isVerificationDone ? () {
+                      print("Verified");
+                    } : null,
+                    child: const Text(
+                      'Confirm',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  );
                 },
-                child: Text(
-                  'Confirm',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
+              )
             ),
-            SizedBox(
+            const SizedBox(
               height: 40,
             ),
             
