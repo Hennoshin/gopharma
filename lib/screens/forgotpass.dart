519f1b2291c61d946d78cf3d9b35a848fee7666c
Hennoshin <m.irfan.hilmi@gmail.com>
2022-05-11 21:34:21 +0700

    Initial commit

---
+++ lib/screens/forgotpass.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:gopharma/screens/register/signup.dart';
+import 'package:gopharma/viewmodel/login_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+class ForgotPass extends StatefulWidget {
+  const ForgotPass({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPassState createState() => _ForgotPassState();
+}
+
+class _ForgotPassState extends State<ForgotPass> {
+  void _resetPassword(BuildContext context) async {
+    await context.read<LoginViewModel>().resetPassword();
+
+    ScaffoldMessenger.of(context).showSnackBar(TextSnackBar(text: "Reset email has been sent"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan.shade300,
+      appBar: AppBar(
+        title: const Text("Forgot Password"),
+        backgroundColor: Colors.cyan.shade400,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset('asset/images/login.png')),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 100, right: 100, top: 15, bottom: 0),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 100, right: 100, top: 15, bottom: 0),
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none),
+                    filled: true,
+                    fillColor: Colors.white,
+                    label: const Center(
+                      child: Text('Put Your Email Here to reset your Password'),
+                    ),
+                    hintText: 'ex: johndoe@mail.domain'),
+                onChanged: (value) {
+                  context.read<LoginViewModel>().email = value;
+                },
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 100, right: 100, top: 15, bottom: 20),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 100, right: 100, top: 0, bottom: 40),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.pink, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  _resetPassword(context);
+                },
+                child: const Text(
+                  'Reset Passwword',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:gopharma/screens/forgotpass.dart';
+import 'package:gopharma/screens/register/signup.dart';
+import 'package:gopharma/viewmodel/login_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+import '../routes_name.dart';
+
+class LoginDemo extends StatefulWidget {
+  const LoginDemo({Key? key}) : super(key: key);
+
+  @override
+  _LoginDemoState createState() => _LoginDemoState();
+}
+
+class _LoginDemoState extends State<LoginDemo> {
+  void _login(BuildContext context) async {
+    await context.read<LoginViewModel>().login();
+
+    String? error = context.read<LoginViewModel>().errorMessage;
+    if (error != null) {
+      ScaffoldMessenger.of(context).showSnackBar(TextSnackBar(text: error));
+      return;
+    }
+
+    Navigator.pushReplacementNamed(context, routeHome);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan.shade300,
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 400,
+                    height: 300,
+                    child: Image.asset('asset/images/login.png')),
+
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 100, right: 100, top: 15, bottom: 0
+              ),
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    label: const Center(
+                      child: Text('Email'),
+                    ),
+                    hintText: 'Input Email'),
+                onChanged: (value) {
+                  context.read<LoginViewModel>().email = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 100, right: 100, top: 15, bottom: 20),
+              child: TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    label: const Center(
+                      child: Text('Password'),
+                    ),
+                    hintText: 'Input Password'),
+                onChanged: (value) {
+                  context.read<LoginViewModel>().password = value;
+                },
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                Navigator.pushReplacementNamed(context, routeRegister);
+              },
+
+              child: const Text(
+                "Don't Have an Account? Register",
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, routeResetPassword);
+              },
+              child: const Text(
+                'Forgot Your Password',
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              margin: const EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  color: Colors.pink, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+
+                onPressed: (){
+                  _login(context);
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
